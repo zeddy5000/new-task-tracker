7c49e970a40e6456d155a6529d694a665fc01b93
zeddy5000 <103764641+zeddy5000@users.noreply.github.com>
2023-02-28 14:46:05 +0100

    changes to api

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ const App = ()=>{
   const [tasks,setTasks] = useState([
 ])
 useEffect(()=>{
-  const fetchTasks = async ()=>{
-    const res = await fetch('http//localhost:5000/tasks')
-    const data = await res.json()
-    console.log(data)
+  const getTasks = async ()=>{
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
   }
-  fetchTasks()
+  getTasks()
 },[])
+const fetchTasks = async ()=>{
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+  return data
+}
 const [showAddTask,setShowAddTask] = useState(false)
 const addTask = (task)=>{
   const id = Math.floor(Math.random() *10000) + 1
